dc97540036b371ee273d8367825ed77acfadb900
Nikola Stojiljkovic <nikolavla@gmail.com>
2021-09-12 20:15:00 +0100

    Create a project and define CLI commands

---
+++ src/commands/mod.rs
@@ -0,0 +1,4 @@
+pub mod client_command;
+pub mod server_command;
+
+pub use self::{client_command::*, server_command::*};
---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use clap::{App, AppSettings};
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let matches = App::new("learncoin")
+        .about("LearnCoin blockchain CLI tools.")
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .subcommand(learncoin_lib::commands::server_command())
+        .subcommand(learncoin_lib::commands::client_command())
+        .get_matches();
+
+    if let Some(ref matches) = matches.subcommand_matches("server") {
+        learncoin_lib::commands::run_server_command(&matches)
+    } else if let Some(ref matches) = matches.subcommand_matches("client") {
+        learncoin_lib::commands::run_client_command(&matches)
+    } else {
+        panic!("Should report help.");
+    }
+}
