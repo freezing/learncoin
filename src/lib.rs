beb252d93878644cb5e72bdadab1a4c3bdac5ac8
Nikola Stojiljkovic <nikolavla@gmail.com>
2021-09-26 20:33:22 +0100

    Mining, coinbase and proof-of-work

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,10 @@ pub mod commands;
 pub mod hash;
 pub mod merkle_tree;
 pub mod orphan_blocks;
+pub mod proof_of_work;
 pub mod transaction;
 
 pub use self::{
-    block::*, blockchain::*, hash::*, merkle_tree::*, orphan_blocks::*, transaction::*,
+    block::*, blockchain::*, hash::*, merkle_tree::*, orphan_blocks::*, proof_of_work::*,
+    transaction::*,
 };
